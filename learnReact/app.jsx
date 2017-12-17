69798db7e44c14db47201e058b36ab4a4c706e52
Ian <yt2015.97@gmail.com>
2017-12-17 13:58:33 +0800

    class component

--- learnReact/app.jsx
+++ learnReact/app.jsx
@@ -1,18 +1,40 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-function tick(){
-    const element = (
-        <div>
-            <h1>hello, world</h1>
-            <h2>it is {new Date().toLocaleTimeString()}</h2>
-        </div>
-    );
+class Clock extends React.Component {
 
-    ReactDOM.render(
-        element,
-        document.getElementById('root')
-    );
+    constructor(props) {
+        super(props)
+        this.state = {date: new Date()}
+    }
+
+    componentDidMount() {
+        this.timerID = setInterval(
+            () => this.tick(),
+            1000
+        )
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID)
+    }
+
+    tick() {
+        this.setState({
+            date: new Date()
+        })
+    }
+    render() {
+        return (
+            <div>
+                <h1>hello</h1>
+                <h2> it is {this.state.date.toLocaleTimeString()}</h2>
+            </div>
+        )
+    }
 }
 
-setInterval(tick, 1000)
+ReactDOM.render(
+    <Clock />,
+    document.getElementById('root')
+)
