631d7064b370c5bd742765d9c75f44c52f259e01
Ian <yt2015.97@gmail.com>
2017-12-18 13:42:17 +0800

    if

--- learnReact/app.jsx
+++ learnReact/app.jsx
@@ -17,5 +17,40 @@ function LogouButton(props) {
 }
 
 class LoginControl extends React.Component {
-    
+    constructor(props) {
+        super(props)
+        this.handleLoginClick = this.handleLoginClick.bind(this)
+        this.handleLogouClick = this.handleLogouClick.bind(this)
+        this.state = {isLoggedIn: true}
+    }
+
+    handleLoginClick() {
+        this.setState({isLoggedIn: true})
+    }
+
+    handleLogouClick() {
+        this.setState({isLoggedIn: false})
+    }
+
+    render() {
+        const isLoggedIn = this.state.isLoggedIn
+
+        let button = null
+        if(isLoggedIn){
+            button = <LogouButton onClick={this.handleLogouClick} />
+        }else {
+            button = <LoginButton onClick={this.handleLoginClick} />
+        }
+
+        return (
+            <div>
+                {button}
+            </div>
+        )
+    }
 }
+
+ReactDOM.render(
+    <LoginControl />,
+    document.getElementById('root')
+)
