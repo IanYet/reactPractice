fa52580c36c076b70e424f30215bc4aa7497649e
Ian <yt2015.97@gmail.com>
2017-12-18 16:50:09 +0800

    temp

---
+++ learnReact/app40.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class BindData extends React.Component{
+    constructor (props) {
+        super(props)
+
+        this.state = {value: ''}
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleChange(e) {
+        this.setState({value: e.target.value.toUpperCase()})
+    }
+
+    render() {
+        return (
+            <div className={this.props.className}>
+                <p>{this.state.value}</p>
+                <input type="text" onChange={this.handleChange} value={this.state.value}/>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <BindData className='hello'/>,
+    document.getElementById('root')
+)
