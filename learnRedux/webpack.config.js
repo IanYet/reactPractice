c2781f1d1d922166b57c6a34888e72b37ad7e27d
Ian <yt2015.97@gmail.com>
2017-12-21 22:45:20 +0800

    redux config

---
+++ learnRedux/webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+
+    entry: './app.jsx',
+
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: "app.js"
+    },
+
+    resolve: {
+        alias: {
+            
+        }
+    },
+
+    module: {
+        rules: [
+            {
+				test: /\.(js|jsx)$/,
+				exclude: /(node_modules|bower_components)/,
+				use: ['babel-loader']
+            }
+        ]
+    },
+
+    devServer: {
+        contentBase: path.join(__dirname, "/dist"),
+		historyApiFallback: {
+            disableDotRule: true,
+            index: 'app.html',
+			rewrites: [
+                { from: /./, to: 'app.html' }
+			]
+		},
+		noInfo: false
+    },
+    
+    devtool: "source-map"
+}
