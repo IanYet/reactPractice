9fb1ca65ccdc8ce8229066de4cd47bdafd42b137
Ian <yt2015.97@gmail.com>
2017-12-25 19:28:26 +0800

    ref

--- learnReact/app.jsx
+++ learnReact/app.jsx
@@ -1,6 +1,32 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-//it is not necessary for js at all
-import PropTypes from 'prop-types'
+class CustomTextInput extends React.Component{
+    constructor(props){
+        super(props)
+        this.focus = this.focus.bind(this)
+    }
 
+    componentDidMount() {
+        this.textInput.focus()
+    }
+
+    focus() {
+        console.log(this.textInput)
+        return this.textInput.focus()
+    }
+
+    render() {
+        return (
+            <div>
+                <input id="01" type="text" ref={(input) => {this.textInput = input}} />
+                <input type="button" value="focus the text input" onClick={this.focus}/>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <CustomTextInput />,
+    document.getElementById('root')
+)
---
+++ learnReact/app60.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+//it is not necessary for js at all
+import PropTypes from 'prop-types'
+
