26c3243b413ecd91ebcbe89b4e9e8eccf1fe513b
Ian <yt2015.97@gmail.com>
2018-01-04 10:49:38 +0800

    basic HOC

--- learnReact/app.jsx
+++ learnReact/app.jsx
@@ -1,48 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const DataSource = {
-    nameStore: [],
-    propStore: [],
-    get name() { return this.nameStore},
-    set name(val) { 
-        this.nameStore.push(val)
-        if(this.handle){
-            this.handle()
-        }
-    },
-    get prop() { return this.propStore},
-    set prop(val) {
-        if(this.handle){
-            this.handle
-        }
-        this.propStore.push(val)
+const hocFactory = WrappedComponent => class HOC extends React.Component{
+    render() {
+        return(
+            <div>
+                <p>hoc wrapper</p>
+                <WrappedComponent {...this.props} />
+            </div>
+        )
     }
 }
 
-DataSource.addChangeListener = (handle) => {
-    DataSource.handle = handle 
-}
-
-function withSubcription(WrappedComponent, selectData) {
-    return class extends React.Component {
-        constructor(props){
-            super(props)
-
-            this.handleChange = this.handleChange.bind(this)
-            this.state = {
-                data: selectData(DataSource, props)
-            }
-        }
-
-        componentDidMount() {
-            DataSource.addChangeListener(this.handleChange)
-        }
-
-        handleChange() {
-            this.setState({
-                data: selectData(DataSource, this.props)
-            })
-        }
+class Demo extends React.Component{
+    render() {
+        return(
+            <p>this is a gen component</p>
+        )
     }
 }
+
+const EnhanceCom = hocFactory(Demo)
+
+ReactDOM.render(
+    <EnhanceCom />,
+    document.getElementById('root')
+)
