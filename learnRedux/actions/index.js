986f8e3c79dc960803a875e7e673dc1dd7fbcf39
Ian <yt2015.97@gmail.com>
2018-01-05 17:05:40 +0800

    redux begin

---
+++ learnRedux/actions/index.js
@@ -0,0 +1,19 @@
+let nextTodoId = 0
+
+const addTodo = text => ({
+    type: 'ADD_TODO',
+    id: nextTodoId++,
+    text
+})
+
+const setVisibilityFilter = filter => ({
+    type: 'SET_VISIBILITY_FILTER',
+    filter
+})
+
+const toggleTodo = id => ({
+    type: 'TOGGLE_TODO',
+    id
+})
+
+export {addTodo, setVisibilityFilter, toggleTodo}
